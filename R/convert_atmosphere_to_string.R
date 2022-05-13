7d184c61fb1145fad92a944eb22c3501cfd128bc
mrustl <michael.rustler@kompetenz-wasser.de>
2022-05-13 13:50:54 +0200

    fix string creation for ATMOSPHERE parameters

--- R/convert_atmosphere_to_string.R
+++ R/convert_atmosphere_to_string.R
@@ -1,12 +1,28 @@
 #' Convert Atmosphere to String
 #'
 #' @param atm  atm as retrieved by \code{prepare_atmosphere_data}
-#'
-#' @return tibble with strings padded properly for Hydrus1D
+#' @param round_digits digits used for rounding values (default: 2) of all columns
+#' besides "tAtm"
+#' @param remove_scientific if TRUE scientific notation of numbers is removed,
+#' otherwise not (default: TRUE)
+#' @return properly padded string of atmosphere variables for Hydrus1D input file
 #' @export
 #' @importFrom stringr str_pad
-#'
-convert_atmosphere_to_string <- function(atm) {
+convert_atmosphere_to_string <- function(atm,
+                                         round_digits = 2,
+                                         remove_scientific = TRUE) {
+
+
+  if(remove_scientific) {
+  ### Get default options
+  opts.default <- options()
+
+  ### get rid of scientific notation
+  options(scipen = 999)
+
+  # Reset to defaults:
+  options(opts.default)
+}
 
   pad_short <- 11
   pad_long <- 12
@@ -14,7 +30,13 @@ convert_atmosphere_to_string <- function(atm) {
   headers_short <- "tAtm"
   headers_long <- get_atmosphere_headers()[!get_atmosphere_headers() %in% headers_short]
 
-  atm[headers_short] <- (lapply(atm[headers_long],
+
+
+  atm[headers_long] <- (lapply(atm[headers_long],
+                               function(x) round(x, round_digits)))
+
+
+  atm[headers_short] <- (lapply(atm[headers_short],
                                 function(x) stringr::str_pad(x,
                                                              pad_short,
                                                              "left")))
@@ -33,8 +55,10 @@ convert_atmosphere_to_string <- function(atm) {
                                              pad_short,
                                              "left")
 
-  atm
-  #sprintf("%s\n%s",
-  #        paste0(names(atm), collapse = ""),
+txt <-  sprintf("%s\n%s",
+          paste0(names(atm), collapse = ""),
+          paste0(apply(atm, 1, function(x) paste0(x, collapse = "")), collapse = "\n"))
+
+
 
 }

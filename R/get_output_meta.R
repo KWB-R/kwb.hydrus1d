b5fdb822277ccf3b365a804e5b6e78f77627e479
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-05-26 23:06:36 +0200

    Do not use getAttribute()
    "meta_general" may be NULL when setting the
    metadata attributes. Then, the attribute is not
    set and getAttribute() would complain.

--- R/get_output_meta.R
+++ R/get_output_meta.R
@@ -4,7 +4,6 @@
 #'
 #' @return returns metainformation list with sublists "general" and "units"  of
 #' imported 'A_LEVEL.out' or 'T_LEVEL.out' file
-#' @importFrom kwb.utils getAttribute
 #' @export
 #' @examples
 #' test_file <- function(x) system.file("extdata/model/test", x, package = "kwb.hydrus1d")
@@ -14,7 +13,7 @@
 #' get_output_meta(tlevel)
 get_output_meta <- function(output) {
   list(
-    general = kwb.utils::getAttribute(output, "meta_general"),
-    units = kwb.utils::getAttribute(output, "meta_units")
+    general = attr(output, "meta_general"),
+    units = attr(output, "meta_units")
   )
 }

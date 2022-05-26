661252b7e79c5635b74de777ce567996667b5029
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-05-26 08:48:35 +0200

    Clean defaults_atmosphere()
    - reindent
    - do not use double quotes for list element
      names (or is there a reason to do so?)
    - simplify name (defaults_list -> defaults)

--- R/defaults-atmosphere.R
+++ R/defaults-atmosphere.R
@@ -1,4 +1,3 @@
-
 #' Defaults for Atmosphere
 #'
 #' @param Prec Precipitation (default: 0)
@@ -20,35 +19,37 @@
 #' @importFrom tibble as_tibble
 #' @examples
 #' defaults_atmosphere()
-defaults_atmosphere <- function(Prec = 0L,
-                                rSoil = 0L,
-                                rRoot = 0L,
-                                hCritA = 100000L,
-                                rB = 0L,
-                                hB = 0L,
-                                ht = 0L,
-                                tTop = 0L,
-                                tBot = 0L,
-                                Ampl = 0L,
-                                cTop = 0L,
-                                cBot = 0L,
-                                RootDepth = 0L) {
- defaults_list <- list(
-    "Prec" = Prec,
-    "rSoil" = rSoil,
-    "rRoot" = rRoot,
-    "hCritA" = hCritA,
-    "rB" = rB,
-    "hB" = hB,
-    "ht" = ht,
-    "tTop" = tTop,
-    "tBot" = tBot,
-    "Ampl" = Ampl,
-    "cTop" = cTop,
-    "cBot" = cBot,
-    "RootDepth" = RootDepth
+defaults_atmosphere <- function(
+  Prec = 0L,
+  rSoil = 0L,
+  rRoot = 0L,
+  hCritA = 100000L,
+  rB = 0L,
+  hB = 0L,
+  ht = 0L,
+  tTop = 0L,
+  tBot = 0L,
+  Ampl = 0L,
+  cTop = 0L,
+  cBot = 0L,
+  RootDepth = 0L
+)
+{
+  defaults <- list(
+    Prec = Prec,
+    rSoil = rSoil,
+    rRoot = rRoot,
+    hCritA = hCritA,
+    rB = rB,
+    hB = hB,
+    ht = ht,
+    tTop = tTop,
+    tBot = tBot,
+    Ampl = Ampl,
+    cTop = cTop,
+    cBot = cBot,
+    RootDepth = RootDepth
   )
 
- tibble::as_tibble(t(unlist(defaults_list)))
-
+  tibble::as_tibble(t(unlist(defaults)))
 }

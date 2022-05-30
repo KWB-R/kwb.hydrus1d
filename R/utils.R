9b78cfc432efff6d71ca8980e129f98baac293d3
mrustl <michael.rustler@kompetenz-wasser.de>
2022-05-30 15:00:00 +0200

    start SELECTOR writting reading function

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,20 @@ stop_formatted <- function(x, ..., call. = FALSE)
 {
   stop(sprintf(x, ...), call. = call.)
 }
+
+
+to_r_truefalse <- function(txt) {
+
+  txt <- stringr::str_replace_all(txt, pattern = "^t$", replacement = "TRUE")
+  txt <- stringr::str_replace_all(txt, pattern = "^f$", replacement = "FALSE")
+  txt
+}
+
+to_fortran_truefalse <- function(txt) {
+
+  txt <- stringr::str_replace_all(txt, pattern = "^TRUE$", replacement = "t")
+  txt <- stringr::str_replace_all(txt, pattern = "^FALSE$", replacement = "f")
+  txt
+}
+
+

adaff030b258f513305af24737615f77ab8c70f0
mrustl <michael.rustler@kompetenz-wasser.de>
2024-07-08 16:35:52 +0200

    Improve read_profile and start on write_profile

--- R/read_profile.R
+++ R/read_profile.R
@@ -10,7 +10,21 @@ read_profile <- function(path) {
 
   lines <- readLines(path)
 
+  pcp_idx <- grep("Pcp_File_Version", lines)
+
   header_idx <- grep("x", lines)
+
+  number_of_materials <- as.integer(lines[pcp_idx+1])
+
+  mat_props <- lines[(pcp_idx+2):(header_idx-1)] %>%
+    stringr::str_trim() %>%
+    stringr::str_replace_all("\\s{1,20}", ",") %>%
+    stringr::str_split(",", simplify = TRUE) %>%
+    as.data.frame()
+
+  mat_props <- lapply(mat_props, as.numeric) %>%  dplyr::bind_rows()
+  names(mat_props) <- c("mat_id", "mat_depth", "mat_prop3", "mat_pro4")
+
 
   dat <- lines[(header_idx+1):length(lines)] %>%
     stringr::str_trim() %>%
@@ -41,9 +55,33 @@ read_profile <- function(path) {
 
   path_profile <- file.path(tempdir(), "profile.csv")
 
+
+  obsnodes <- list(n = 0,
+                   ids = NULL)
+
+  if (which(ncols == 2) > 0) {
+    idx_obsnodes <- which(ncols == 2)
+    if(idx_obsnodes != length(ncols)) {
+
+    n_obsnodes <- as.integer(dat[idx_obsnodes])
+    obs_node_ids <- stringr::str_split(dat[length(dat)],
+                                       pattern = ",",
+                                       simplify = TRUE) %>%
+      as.integer()
+
+    obsnodes <- list(n = n_obsnodes,
+                     ids = obs_node_ids)
+    }
+  }
+
   c(paste0(header_clean, collapse = ","),
     dat[ncols == median(ncols)]) %>%
     writeLines(path_profile)
 
-  readr::read_csv(path_profile)
+  list(mat_props = mat_props,
+       profile = readr::read_csv(path_profile),
+       obsnodes = obsnodes)
+
+
 }
+
